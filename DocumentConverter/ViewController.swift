b3a227038b437288332ea22735ffd9ced3c94b41
Balint Halasz <balint.halasz@accedo.tv>
2024-10-03 11:16:26 +0200

    Initial Commit

---
+++ DocumentConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DocumentConverter
+//
+//  Created by Balint Halasz on 03/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DocumentConverterTests/DocumentConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  DocumentConverterTests.swift
+//  DocumentConverterTests
+//
+//  Created by Balint Halasz on 03/10/2024.
+//
+
+import Testing
+@testable import DocumentConverter
+
+struct DocumentConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
